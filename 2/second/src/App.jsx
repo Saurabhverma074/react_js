98795902af36951e76c9e5cf4687c6c5d9016d9c
Saurabhverma074 <saurabhverma1051@gmail.com>
2026-01-12 22:13:30 +0530

    2

---
+++ 2/second/src/App.jsx
@@ -0,0 +1,16 @@
+import New from "./new";
+
+
+function App() {
+  
+
+  return (
+    <>
+      <New/>
+
+      <h2>kaise hai sab loog bhai</h2>
+    </>
+  )
+}
+
+export default App
---
+++ 2/second/src/new.jsx
@@ -0,0 +1,7 @@
+function New(){
+    return(
+    <h1>good morning everyone</h1>
+    )
+};
+
+export default New
